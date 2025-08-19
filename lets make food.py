78347da86974ff2210257052b1d494ff1e945fa3
Subway Sniffers <sfiuarhdn@gmail.com>
2025-08-19 14:11:09 +0100

    Add files via upload

---
+++ lets make food.py
@@ -0,0 +1,101 @@
+from pynput.keyboard import Key, KeyCode, Listener
+import random
+import sys
+
+class vec: # my beloved laziness to import smth else
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return vec(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+        return vec(self.x - other.x, self.y - other.y)
+    def __mul__(self, other): # dot product
+        return vec(self.x * other.x, self.y * other.y)
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
+    def __iadd__(self, other):
+        return vec(self.x + other.x, self.y + other.y)
+    def __isub__(self, other):
+        return vec(self.x - other.x, self.y - other.y)
+    def __imul__(self, other):
+        return vec(self.x * other.x, self.y * other.y)
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+dirs = {
+    'w': vec(0, -1),
+    'a': vec(-1, 0),
+    's': vec(0, 1),
+    'd': vec(1, 0)
+}
+
+
+
+gameData = {}
+
+pos = vec(0, 0)
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.mined = False
+
+        rand = random.randint(0, 99)
+        if rand < 75:
+            self.tile = 'stone'
+            self.code = '\x1b[48;5;242;38;5;240m'
+        elif rand < 87:
+            self.tile = 'coal'
+            self.code = '\x1b[48;5;233;38;5;235m'
+        elif rand < 96:
+            self.tile = 'gold'
+            self.code = '\x1b[48;5;226;38;5;220m'
+        else:
+            self.tile = 'diamond'
+            self.code = '\x1b[48;5;87;38;5;80m'
+    def printMe(self):
+        return self.code + (' ' * self.mined + '#' * (not self.mined))
+
+def update(dir):
+    global gameData, pos
+    pos = pos + dir
+    message = '#======#==========================#\n∥ Game ∥ WASD to move ESC to quit ∥\n#======#==========================#\n∥'
+    for j in range(pos.y - 5, pos.y + 6):
+        for i in range(pos.x - 5, pos.x + 6):
+            if not vec(i, j) in gameData:
+                gameData[vec(i, j)] = Tile(i, j)
+            if vec(i, j) != pos:
+                message += gameData[vec(i, j)].printMe() * 3
+            else:
+                message += '\x1b[48;5;40;38;5;118m @ '
+                gameData[vec(i, j)].mined = True
+        message += '\x1b[0m∥\n'
+        if j != pos.y + 5:
+            message += '∥'
+        else:
+            message += '#=================================#\n'
+
+
+    sys.stdout.write('\r\x1b[H' + message)
+    sys.stdout.flush()
+
+update(vec(0, 0))
+
+def captDir(key):
+    if key == Key.esc:
+        sys.exit()
+    for d, v in dirs.items():
+        if key == KeyCode.from_char(d):
+            update(v)
+
+
+listener = Listener(on_press=captDir
+)
+listener.start()
+listener.join()
