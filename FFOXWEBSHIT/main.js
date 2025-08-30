dda2bc2f08800b6e40ac54854683e92eb159ccd3
Subway Sniffers <sfiuarhdn@gmail.com>
2025-08-30 12:05:12 +0100

    Update main.js

--- FFOXWEBSHIT/main.js
+++ FFOXWEBSHIT/main.js
@@ -76,7 +76,7 @@ framey.src = 'walk anim/sprite_3.png';
 let ccc = 0;
 
 let xPosition = 10;
-let yPosition = canvas.height - 150;
+let yPosition = canvas.height - 120;
 let xVelocity = 1;
 let yVelocity = 0;
 let onFloor = true;
